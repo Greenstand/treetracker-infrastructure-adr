699c0e8a7f536475b23688b9b59a2c4bc3cd58db
Zaven Arra <matthew.fullstackgis@gmail.com>
2021-05-05 19:10:15 -0700

    Add some details for running and verifying

--- upload.sh
+++ upload.sh
@@ -4,3 +4,10 @@ CAPTURE_UUID_VALUE=`uuid`
 REGISTRATION_UUID_VALUE=`uuid`
 sed "s/CAPTURE_UUID_VALUE/$CAPTURE_UUID_VALUE/" template/captures.json > prepared/captures.json
 sed "s/REGISTRATION_UUID_VALUE/$REGISTRATION_UUID_VALUE/" template/registrations.json > prepared/registrations.json
+
+#upload it
+aws s3 cp --profile treetracker-test-env prepared/captures.json s3://treetracker-test-batch-uploads
+aws s3 cp --profile treetracker-test-env prepared/registrations.json s3://treetracker-test-batch-uploads
+
+echo "capture uuid: $CAPTURE_UUID_VALUE"
+echo "registration uuid: $REGISTRATION_UUID_VALUE"
